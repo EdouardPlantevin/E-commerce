4882b0b1c4ffee621f6e7a5e67f3f1f181b66d74
edouardplantevin <plantevin.contact@gmail.com>
2021-01-05 17:17:32 +0100

    add form

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Cocur\Slugify\Slugify;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class CategoryController extends AbstractController
+{
+
+    private $entityManager;
+    private $slugger;
+
+    public function __construct(EntityManagerInterface $entityManagerInterface, SluggerInterface $sluggerInterface)
+    {
+        $this->entityManager = $entityManagerInterface;
+        $this->slugger = $sluggerInterface;
+    }
+    /**
+     * @Route("/admin/category/create", name="category_create")
+     */
+    public function create(Request $request): Response
+    {
+        $form = $this->createForm(CategoryType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $category = $form->getData();
+            $category->setSlug(strtolower(strtolower($this->slugger->slug($category->getName()))));
+
+            $this->entityManager->persist($category);
+            $this->entityManager->flush(); 
+            
+            return $this->redirectToRoute("home");
+        }
+
+        return $this->render('category/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/category/{id}/edit", name="category_edit")
+     */
+    public function edit($id, CategoryRepository $categoryRepository, Request $request): Response
+    {
+        $category = $categoryRepository->find($id);
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $category->setSlug(strtolower($this->slugger->slug($category->getName())));
+            $this->entityManager->flush();
+            
+            $this->redirectToRoute("home");
+        }
+
+        if(!$category) {
+            throw $this->createNotFoundException("La categorie n'existe pas");
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Form/ProductType.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use App\Entity\Category;
+use App\Form\DataTransformer\CentimesTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('name', TextType::class, [
+            'label' => 'Nom du produit',
+            'attr' => [
+                'placeholder' => 'Taper le nom du produit'
+            ]
+        ])
+        ->add('shortDescription', TextareaType::class, [
+            'label' => 'Description courte',
+            'attr' => [
+                'placeholder' => 'Taper une description courte'
+            ]
+        ])
+        ->add('price', MoneyType::class, [
+            'label' => 'Prix',
+            'divisor' => 100,
+            'attr' => [
+                'placeholder' => 'Taper le prix du produit'
+            ]
+        ])
+        ->add('mainPicture', UrlType::class, [
+            'label' => 'Image du produit',
+            'attr' => [
+                'placeholder' => 'Placer une url d\'image'
+            ]
+        ])
+        ->add('category', EntityType::class, [
+            'class' => Category::class,
+            'label' => 'Catégorie',
+            'placeholder' => '-- Choisir une categorie --',
+        ]);
+
+        // $builder->get('price')->addModelTransformer(new CentimesTransformer);
+
+        // $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event)
+        // {
+        //     /** @var Product */
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null)
+        //     {
+        //         $product->setPrice($product->getPrice() * 100);
+        //     }
+        // });
+
+        // $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) 
+        // {
+        //     $form = $event->getForm();
+
+        //     /** @var Product */
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null)
+        //     {
+        //         $product->setPrice($product->getPrice() / 100);
+        //     }
+            
+        //     //dd($product);
+
+        // });
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
